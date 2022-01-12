386d1302655d0243cb544d82445b6d94458abc0d
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-12 15:40:49 +0530

    learn about pure-components and how it work.

---
+++ src/components/pure-components/ParentComp.js
@@ -0,0 +1,36 @@
+import React, { Component, PureComponent } from "react";
+import PureComp from "./PureComp";
+import RegComp from "./RegComp";
+
+class ParentComp extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "kashyap",
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        name: "kashyap",
+      });
+    }, 2000);
+  }
+
+  render() {
+    console.log(
+      "******************** Parent Component Render ********************"
+    );
+    return (
+      <div>
+        Parent Component
+        <RegComp name={this.state.name} />
+        <PureComp name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+export default ParentComp;
