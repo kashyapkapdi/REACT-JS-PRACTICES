89de7dcb1844875d50b6cfdb8d630f020cdefdca
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-17 13:23:45 +0530

    Learn how to use useState with array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ClassCounter from "./components/useStateHooks/ClassCounter";
 import FunctionCounter from "./components/useStateHooks/FunctionCounter";
 import FunctionCounterObject from "./components/useStateHooks/FunctionCounterObject";
+import FunctionCounterArray from "./components/useStateHooks/FunctionCounterArray";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <ClassCounter />
         <FunctionCounter />
         <FunctionCounterObject />
+        <FunctionCounterArray />
       </div>
     );
   }
